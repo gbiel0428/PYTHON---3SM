34a10a11b893df8d0238a8ff391c33d6098e7ed3
gbiel0428 <gabriiiel2807@gmail.com>
2026-08-18 18:34:40 -0300

    Atualizar o atv02.py

--- AULA3/atv02.py
+++ AULA3/atv02.py
@@ -1,56 +1,40 @@
-import os 
+import os
+
 os.system("cls")
 
-N1 = float(input("Informe o 1 Número: "))
-N2 = float(input("Informe o 2 Número: "))
+N1 = float(input("Informe o 1º número: "))
+N2 = float(input("Informe o 2º número: "))
 
 print("""
-    Codigo | Operação
-    1      |  +
-    2      |  -
-    3      |  /
-    4      |  *
-    5      |  STOP""")
-
+    Código | Operação
+    1      | +
+    2      | -
+    3      | /
+    4      | *
+    5      | STOP
+""")
 
 while True:
-    escolha = input("Informe o Codigo da operação: ")
-    
+    escolha = input("Informe o código da operação: ")
+
     if escolha == "1":
-        print(f"A Soma dos Números é: {N1 + N2}.")
+        print(f"A soma dos números é: {N1 + N2}.")
+
     elif escolha == "2":
-        print(f"A subtração dos Números é: {N1 - N2}.")
+        print(f"A subtração dos números é: {N1 - N2}.")
+
     elif escolha == "3":
-        print(f"A Divisão dos Números é: {N1 / N2}.")
+        if N2 == 0:
+            print("Não é possível dividir por zero.")
+        else:
+            print(f"A divisão dos números é: {N1 / N2}.")
+
     elif escolha == "4":
-        print(f"A Multiplicação dos Números é: {N1 * N2}.")
+        print(f"A multiplicação dos números é: {N1 * N2}.")
+
     elif escolha == "5":
-        print("Operação Encerrada.")
+        print("Operação encerrada.")
         break
+
     else:
-        print("Codigo Invalido.")
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-# match opcao:
-#     case "1":
-#         print(f"A soma dos Numeros é : {N1 + N2}.")
-#     case "2":
-#         print(f"A subtração dos Números é: {N1 - N2}.")
-#     case "3":
-#         print(f"A divisão dos Números é: {N1 / N2}.")
-#     case "4":
-#         print(f"A Multiplicação dos Números é: {N1 * N2}.")
-#     case _:
-#         print("Operação invalida")
+        print("Código inválido.")
